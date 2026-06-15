581ac3cad4091dde85d8247394901471c25de118
Ashwet Kini <ashukini3@gmail.com>
2026-06-15 22:34:48 +0530

    updated mobile no

--- src/components/ui/HaveQuestionsSection.jsx
+++ src/components/ui/HaveQuestionsSection.jsx
@@ -37,7 +37,7 @@ const cards = [
     title: 'Call Us',
     description: 'Call us for quick assistance.',
     action: 'link',
-    target: 'tel:+918329833526'
+    target: 'tel:+919763567410'
   }
 ]
 
